eab1598dee8df792f8b38460790c1fc2862b959b
Jacob Chen <jacobpchen@gmail.com>
2020-12-29 07:43:22 -0500

    completed question 05

--- 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
+++ 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
@@ -1,6 +1,12 @@
 function countOfAllBooleansAndStrings(arr) {
-  // Insert code here;
+    // Insert code here;
+    var count = 0
+    for (i = 0; i < arr.length; i++) {
+        if (typeof arr[i] == 'string' | typeof arr[i] == 'boolean') {
+            count++
+        }
+    }
+    return count
 }
-
 // Do not edit this line;
 module.exports = countOfAllBooleansAndStrings;
