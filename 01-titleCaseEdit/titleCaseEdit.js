e1a90c7cee19ec5e169d1df3c6fc6639bd334a78
Jacob Chen <jacobpchen@gmail.com>
2020-12-28 20:49:46 -0500

    completed question 01

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,5 +1,11 @@
 function titleCaseEdit(title) {
   // Insert code here;
+  var splitTitle = title.split(' ')
+  for (i = 0; i < splitTitle.length; i++) {
+    splitTitle[i] = splitTitle[i].charAt(0).toUpperCase() + splitTitle[i].substring(1);
+  }
+  return splitTitle.join(' ')
+
 }
 
 // Do not edit this line;
