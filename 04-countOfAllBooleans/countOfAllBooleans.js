4be0f2fd829a145a8cc774b6d5eaf3a196fab657
Jacob Chen <jacobpchen@gmail.com>
2020-12-28 21:20:56 -0500

    completed question 04

--- 04-countOfAllBooleans/countOfAllBooleans.js
+++ 04-countOfAllBooleans/countOfAllBooleans.js
@@ -1,6 +1,13 @@
 function countOfAllBooleans(arr) {
   // Insert code here;
+  var count = 0
+  console.log(arr.length)
+  for (i = 0; i < arr.length; i++)
+    if (arr[i] === true | arr[i] === false)
+      count++
+  return count
 }
 
 // Do not edit this line;
 module.exports = countOfAllBooleans;
+countOfAllBooleans([true, false, true, false, true, 7])
