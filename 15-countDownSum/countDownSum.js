416afcc0591556279030ed45fd22e65bd8e3f3a3
Jacob Chen <jacobpchen@gmail.com>
2021-01-03 13:27:12 -0500

    completed question 15

--- 15-countDownSum/countDownSum.js
+++ 15-countDownSum/countDownSum.js
@@ -1,12 +1,14 @@
 class MySolution {
+
   countDownSum(num) {
     // Insert code here;
-    if (num <= 1)
+    if (num == 1)
       return num;
     else
       return num + this.countDownSum(num - 1)
   }
 }
 
-let studentSolution = new MySolution();
-console.log(studentSolution.countDownSum(10))
+let StudentSolution = MySolution
+module.exports = StudentSolution
+// console.log(StudentSolution.countDownSum(10))
