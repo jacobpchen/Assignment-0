6fe058c6b6364bdf18a8193e3b2c92f4d0873072
Jacob Chen <jacobpchen@gmail.com>
2021-01-04 14:15:15 -0500

    deleted an unused variable

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -2,7 +2,7 @@ function frequencyCounter(word) {
   // Insert code here;
   var letterCount = new Object()
   var letter
-  var count = 0
+
   for (i = 0; i < word.length; i++) {
     if (!(word[i] in letterCount)) {
       letter = word[i]
